49575adc840b59cc14278dd08fa2f8f0ad8aec93
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-06-04 08:28:25 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 import "./App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="container">
       <div className="center">
-        <h1 className="number">0</h1>
+        <h1 className="number">{count}</h1>
         <div className="button-box">
-          <button className="subtract">SUBTRACT</button>
-          <button className="reset">RESET</button>
-          <button className="add">ADD</button>
+          <button className="subtract" onClick={handleSubtract}>
+            SUBTRACT
+          </button>
+          <button className="reset" onClick={handleReset}>
+            RESET
+          </button>
+          <button className="add" onClick={handleAdd}>
+            ADD
+          </button>
         </div>
       </div>
     </div>
